d2a4af18a74757aa8d8207d0e4c187af698c061d
Avishka Medagamagodage <aimedagamagodage2003@gmail.com>
2026-05-13 17:56:52 +0530

    feat: initialize user-service project with REST controller, global exception handler, OpenAPI configuration, and Maven dependencies

--- Backend/user-service/src/main/java/com/avishka/userservice/controller/UserController.java
+++ Backend/user-service/src/main/java/com/avishka/userservice/controller/UserController.java
@@ -1,4 +1,56 @@
 package com.avishka.userservice.controller;
 
+import com.avishka.userservice.dto.UserRequest;
+import com.avishka.userservice.dto.UserResponse;
+import com.avishka.userservice.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponse> createUser(
+            @Valid @RequestBody UserRequest request
+    ) {
+        return new ResponseEntity<>(
+                userService.createUser(request),
+                HttpStatus.CREATED
+        );
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponse> updateUser(
+            @PathVariable Long id,
+            @RequestBody UserRequest request
+    ) {
+        return ResponseEntity.ok(userService.updateUser(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+
+        userService.deleteUser(id);
+
+        return ResponseEntity.ok("User deleted successfully");
+    }
 }
